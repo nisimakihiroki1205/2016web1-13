2b7d4c37e4cfabb4bd2ea0d858b427c9428967fe
nisimakihiroki1205 <g1527055@st.t-kougei.ac.jp>
2016-07-04 17:21:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -12,7 +12,8 @@ var now = new Date();
 var counter = separate_time(now);
 document.getElementById('countdown').textContent =
  counter[5] + '年' +
- counter[5] + '年' +
- counter[5] + '年' +
- counter[5] + '年' +
- counter[5] + '年' +
+ counter[4] + '月' +
+ counter[3] + '日' +
+ counter[2] + '時' +
+ counter[1] + '分' +
+ counter[0] + '秒' +

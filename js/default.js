b14d3cae281969b143b8f04b814aaaf806c94331
nisimakihiroki1205 <g1527055@st.t-kougei.ac.jp>
2016-07-04 17:19:47 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,18 @@
+var separate_time = function(time){
+var sec = time.getSeconds();
+var min = time.getMinutes();
+var hours = time.getHours();
+var days = time.getDate();
+var month = time.getMonth();
+var year = time.getFullYear();
+return[sec,min,hours,days,month,year];
+}
 
+var now = new Date();
+var counter = separate_time(now);
+document.getElementById('countdown').textContent =
+ counter[5] + '年' +
+ counter[5] + '年' +
+ counter[5] + '年' +
+ counter[5] + '年' +
+ counter[5] + '年' +
